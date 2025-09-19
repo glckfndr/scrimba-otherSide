f206bda5181048340654599fc1ecdcf20dd7046c
glckfndr <obulan65@gmail.com>
2025-09-19 15:18:50 +0300

    feat: add getData

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-import http from "node:http";
+import http, { get } from "node:http";
 import { serveStatic } from "./utils/serveStatic.js";
+import { getData } from "./utils/getData.js";
 
 const PORT = 8000;
 
 const __dirname = import.meta.dirname;
+const data = await getData();
+console.log(data);
 
 const server = http.createServer(async (req, res) => {
   await serveStatic(req, res, __dirname);
---
+++ utils/getData.js
@@ -0,0 +1,13 @@
+import path from "node:path";
+import fs from "node:fs/promises";
+
+export async function getData() {
+  const filePath = path.join("data", "data.json");
+  try {
+    const content = await fs.readFile(filePath, "utf-8");
+    return JSON.parse(content);
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
+}
