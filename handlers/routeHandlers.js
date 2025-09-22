b64c1894c90b6fc059cecd4df6dc0b365aea9920
glckfndr <obulan65@gmail.com>
2025-09-22 10:46:53 +0300

    feat: add parseJSONBody for handlePost

--- handlers/routeHandlers.js
+++ handlers/routeHandlers.js
@@ -1,10 +1,14 @@
 import { getData } from "../utils/getData.js";
 import { sendResponse } from "../utils/sendResponse.js";
+import { parseJSONBody } from "../utils/parseJSONBody.js";
+
 export async function handleGet(res) {
   const data = await getData();
   await sendResponse(res, 200, "application/json", JSON.stringify(data));
 }
 
 export async function handlePost(req, res) {
-  console.log("POST request received");
+  const rawBody = await parseJSONBody(req);
+  console.log(rawBody);
+  return rawBody;
 }
---
+++ utils/parseJSONBody.js
@@ -0,0 +1,11 @@
+export async function parseJSONBody(req) {
+  try {
+    let body = "";
+    for await (const chunk of req) {
+      body += chunk;
+    }
+    return JSON.parse(body);
+  } catch (err) {
+    throw new Error(`Invalid JSON format: ${err}`);
+  }
+}
